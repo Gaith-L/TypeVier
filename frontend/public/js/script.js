8578483f210e8beb71661cd4ffb3f6e12c9c3d6e
Gaith-L <gaithlaith66@gmail.com>
2024-11-17 20:07:11 +1300

    Type race improvement: added WPM calculator

--- frontend/public/js/script.js
+++ frontend/public/js/script.js
@@ -1,5 +1,6 @@
-// let words = "The sun dipped below the horizon, casting a warm orange glow across the tranquil lake. Birds chirped their final melodies of the day as the first stars began to twinkle in the twilight sky. A gentle breeze rustled the leaves of the tall trees lining the water's edge, creating a soothing symphony that harmonized with the soft lapping of the waves. In this serene moment, time seemed to stand still, offering a brief escape from the chaos of everyday life It was a perfect reminder of nature's quiet beauty and the peace it could bring to a restless soul."
-let words = "This sentence if for testing."
+let words = "The sun dipped below the horizon, casting a warm orange glow across the tranquil lake. Birds chirped their final melodies of the day as the first stars began to twinkle in the twilight sky. A gentle breeze rustled the leaves of the tall trees lining the water's edge, creating a soothing symphony that harmonized with the soft lapping of the waves. In this serene moment, time seemed to stand still, offering a brief escape from the chaos of everyday life It was a perfect reminder of nature's quiet beauty and the peace it could bring to a restless soul."
+// let words = "This sentence if for testing."
+
 const wordsArray = words.split(" ")
 const wordsArrayLength = wordsArray.length
 
@@ -8,14 +9,19 @@ let htmlWords = htmlWordsContainer.split(' ')
 
 let wordsIndex = 0;
 
-const wordsContainer = document.getElementById('wordsContainer');
+const wordsContainer = document.getElementById('wordsContainer')
 const input = document.getElementById('wordsInput')
+const resultContainer = document.getElementById('resultContainer')
 
 let checkWordInputIntervalId
 let typingTestRunning = false
 
+let timerTens = 0
+let timerSeconds = 0
+
 function checkWordInput() {
-    console.log("checkinginput....")
+    startTimer()
+    console.log(timerSeconds + ":" + timerTens)
     let wordSpans = wordsContainer.querySelectorAll('.word-span')
     wordSpans.forEach(span => {
         span.style.color = ''
@@ -25,12 +31,12 @@ function checkWordInput() {
         clearInterval(checkWordInputIntervalId); // Stop the interval once wordsIndex exceeds array length
         typingTestRunning = false
         wordsIndex = 0
+        resultContainer.innerHTML = (wordsArrayLength / ((timerSeconds + 1 / timerTens) / 60)).toFixed(0)
         return;
     }
     wordSpans[wordsIndex].style.color = 'yellow'
     if (input.value === wordsArray[wordsIndex] + " ") {
         input.value = ""
-        console.log(wordsArray[wordsIndex])
         wordsIndex++
     }
 }
@@ -46,23 +52,24 @@ function highlightWords() {
 document.getElementById("startRaceButton").addEventListener("click", startRace);
 
 function startRace() {
-    console.log("Started race!" + typingTestRunning)
-
     if (typingTestRunning == false) {
         wordsContainer.innerHTML = ""
         wordsArray.forEach((word, idx) => {
-            // console.log(word)
             wordsContainer.innerHTML += `<span class="word-span" id="word-span-id-${idx}">${word} </span>`
         });
-    }
-    typingTestRunning = true
 
+        input.value = ''
+        checkWordInputIntervalId = setInterval(checkWordInput, 10) // TODO: improve this by adding a listener on 'space'
+    }
     input.focus();
-    input.value = ''
-    checkWordInputIntervalId = setInterval(checkWordInput, 10) // TODO: improve this by adding a listener on 'space'
+    typingTestRunning = true
 }
 
-document.getElementById('counterButton').addEventListener('click', function() {
-    count++;
-    document.getElementById('countDisplay').innerText = 'Count: ' + count;
-})
+function startTimer() {
+    timerTens++
+
+    if (timerTens > 99) {
+        timerSeconds++
+        timerTens = 0
+    }
+}
