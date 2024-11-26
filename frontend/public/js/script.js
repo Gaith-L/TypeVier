7422e9dea06a6f6ef04cff93b5b39a0ca7c7b4b9
Gaith-L <gaithlaith66@gmail.com>
2024-11-27 12:39:09 +1300

    added ctrl+backspace & other small improvements

--- frontend/public/js/script.js
+++ frontend/public/js/script.js
@@ -49,15 +49,58 @@ class TypeRacer {
         this.endTimer = () => state.endTime = Date.now()
 
         this.checkLetter = (event) => {
+            event.preventDefault()
+
+            if (state.startTime === null) {
+                this.startRace()
+            }
+            console.log(state.startTime);
+
             let wordsContainer = document.getElementById('wordsContainer')
             let words = wordsContainer.children
 
             const expectedCurrentWord = words[state.currentWordIndex]
-            const lettersInCurrentWord = expectedCurrentWord.querySelectorAll('.letter')
+            let lettersInCurrentWord = expectedCurrentWord.querySelectorAll('.letter')
 
-            if (event.key === 'Backspace') {
-                event.preventDefault()
+            if (event.key === 'Backspace' && event.ctrlKey) {
+                if (state.currentLetterIndex > 0) {
+                    for (let i = state.currentLetterIndex; i > 0; i--) {
+                        if (lettersInCurrentWord[state.currentLetterIndex - 1].classList.contains('extra')) {
+                            lettersInCurrentWord[state.currentLetterIndex - 1].remove()
+                            state.currentLetterIndex--
+                        } else {
+                            state.currentLetterIndex--
+                            lettersInCurrentWord[state.currentLetterIndex].classList.remove("correct", "incorrect")
+                        }
+                    }
+                } else if (state.currentWordIndex > 0 && !(words[state.currentWordIndex - 1].classList.contains("correct")
+                    && words[state.currentWordIndex - 1].classList.contains("typed"))) {
+                        words[state.currentWordIndex].classList.remove("active")
 
+                        // Go back to previous word and change class list
+                        state.currentWordIndex--
+                        state.currentLetterIndex = words[state.currentWordIndex].querySelectorAll('.letter').length
+                        words[state.currentWordIndex].classList.remove("typed")
+                        if (words[state.currentWordIndex].classList.contains("correct")) {
+                            words[state.currentWordIndex].classList.remove("correct")
+                        } else if (words[state.currentWordIndex].classList.contains("incorrect")) {
+                            words[state.currentWordIndex].classList.remove("incorrect")
+                        }
+                        words[state.currentWordIndex].classList.add("active")
+
+                        lettersInCurrentWord = words[state.currentWordIndex].querySelectorAll('.letter')
+
+                        for (let i = state.currentLetterIndex; i > 0; i--) {
+                            if (lettersInCurrentWord[state.currentLetterIndex - 1].classList.contains('extra')) {
+                                lettersInCurrentWord[state.currentLetterIndex - 1].remove()
+                                state.currentLetterIndex--
+                            } else {
+                                state.currentLetterIndex--
+                                lettersInCurrentWord[state.currentLetterIndex].classList.remove("correct", "incorrect")
+                            }
+                        }
+                    }
+            } else if (event.key === 'Backspace') {
                 if (state.currentLetterIndex > 0) {
                     if (lettersInCurrentWord[state.currentLetterIndex - 1].classList.contains('extra')) {
                         lettersInCurrentWord[state.currentLetterIndex - 1].remove()
@@ -66,7 +109,8 @@ class TypeRacer {
                         state.currentLetterIndex--
                         lettersInCurrentWord[state.currentLetterIndex].classList.remove("correct", "incorrect")
                     }
-                } else if (state.currentWordIndex > 0) {
+                } else if (state.currentWordIndex > 0 && !(words[state.currentWordIndex - 1].classList.contains("correct")
+                    && words[state.currentWordIndex - 1].classList.contains("typed"))) {
                     words[state.currentWordIndex].classList.remove("active")
 
                     // Go back to previous word and change class list
@@ -82,7 +126,6 @@ class TypeRacer {
                     words[state.currentWordIndex].classList.add("active")
                 }
             } else if (event.key === ' ') {
-                event.preventDefault()
                 if (state.currentLetterIndex === lettersInCurrentWord.length) {
                     // Remove "active" from current typed word
 
@@ -131,6 +174,11 @@ class TypeRacer {
         }
 
         this.updateCaret = () => {
+            if (state.currentWordIndex === 0 && state.currentLetterIndex === 0) {
+                caret.style.animation = "blink 1s step-end infinite"
+            } else {
+                caret.style.animation = ""
+            }
             let wordsContainer = document.getElementById('wordsContainer')
             let words = wordsContainer.children
 
@@ -139,33 +187,69 @@ class TypeRacer {
 
             if (state.currentLetterIndex < letters.length) {
                 const currentLetter = letters[state.currentLetterIndex]
-                caret.style.top = currentLetter.getBoundingClientRect().top + 5 + "px"
+                caret.style.top = currentLetter.getBoundingClientRect().top + "px"
                 caret.style.left = currentLetter.getBoundingClientRect().left + "px"
             } else {
                 const lastLetter = letters[letters.length - 1]
-                caret.style.top = lastLetter.getBoundingClientRect().top + 5 + "px"
+                caret.style.top = lastLetter.getBoundingClientRect().top + "px"
                 caret.style.left = (lastLetter.getBoundingClientRect().left + lastLetter.offsetWidth) + "px"
             }
         }
 
         this.getWords = () => Array.from(document.querySelectorAll('.word'))
         this.getWordsVisual = () => this._wordsVisual
+
+        this.startRace = () => {
+            state.startTime = Date.now()
+            state.keyHistory = [] // includes both correct and incorrect key input
+            state.wordHistory = []
+            state.keyMistakes = [] // a key mistake is a single wrong input (e 'h' i 'j')
+            state.wordMistakes = [] // a word mistake is when a wrong input is typed and space is clicked to 'submit' (e 'The' i 'Teh')
+            state.currentTypedWord = ''
+            state.currentWordIndex = 0
+            state.currentLetterIndex = 0
+            state.currentWPM = 0 // TODO: probably set a timer event every 1 sec that calls a calcWPM func from TypeRacer obj
+            state.finalWPM = 0
+            state.accuracy = 0
+        }
     }
 }
 
 window.onload = onPageLoad
-window.onresize = onResize
+let resizeTimeout;
+window.addEventListener('resize', () => {
+    caret.classList.add("hidden")
+    game.updateCaret()
+
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => {
+        caret.classList.remove("hidden")
+    }, 200); // Adjust the timeout as needed
+});
+
+let wordsContainer = document.getElementById('wordsContainer')
+wordsContainer.addEventListener('click', () => {
+    input.style.pointerEvents = 'all';
+    input.focus();
+});
 
 input = document.getElementById('wordsInput')
-input.addEventListener('keydown', keyPressHandler)
 
-function onResize() {
-    game.updateCaret()
+input.addEventListener('blur', () => {
+    input.style.pointerEvents = 'none';
+});
+
+startRaceButton = document.getElementById("startRaceButton")
+startRaceButton.onclick = () => {
+    game.startRace()
+    input.value = ''
+    input.focus()
+    onPageLoad()
 }
 
 function onPageLoad() {
-    let wordsContainer = document.getElementById('wordsContainer')
     let wordDivs = wordsContainer.children
+    wordsContainer.replaceChildren()
 
     game.getWordsVisual().forEach(word => {
         const wordDiv = document.createElement('div')
@@ -187,24 +271,25 @@ function onPageLoad() {
     input.focus()
 }
 
-
+input.addEventListener('keydown', keyPressHandler)
 function keyPressHandler(event) {
-    const isLetter = event.key.length === 1
+    const isLetter = event.key.length === 1 && !(event.ctrlKey || event.altKey || event.metaKey)
     const isSpace = event.key === ' '
     const isBackspace = event.key === 'Backspace'
+    const isCtrlAndBackspace = (event.ctrlKey && event.key === 'Backspace')
 
-    if (!(isLetter || isSpace || isBackspace) || event.ctrlKey || event.altKey || event.metaKey) {
+    if (isLetter || isSpace || isBackspace || isCtrlAndBackspace) {
+        console.log(event);
+        game.checkLetter(event)
+        game.updateCaret()
+    } else if (event.altKey || event.metaKey) {
         return
     }
-
-    game.checkLetter(event)
-    game.updateCaret()
-
-    input.value = ''
 }
 
-let words = "The sun dipped below the horizon, casting a warm orange glow across the tranquil lake. Birds chirped their final melodies of the day as the first stars began to twinkle in the twilight sky. A gentle breeze rustled the leaves of the tall trees lining the water's edge, creating a soothing symphony that harmonized with the soft lapping of the waves. In this serene moment, time seemed to stand still, offering a brief escape from the chaos of everyday life It was a perfect reminder of nature's quiet beauty and the peace it could bring to a restless soul."
+// let words = "The sun dipped below the horizon, casting a warm orange glow across the tranquil lake. Birds chirped their final melodies of the day as the first stars began to twinkle in the twilight sky. A gentle breeze rustled the leaves of the tall trees lining the water's edge, creating a soothing symphony that harmonized with the soft lapping of the waves. In this serene moment, time seemed to stand still, offering a brief escape from the chaos of everyday life It was a perfect reminder of nature's quiet beauty and the peace it could bring to a restless soul."
 // let words = "a sentence for testing the program."
+let words = "hastily beneath swimming delicious through banana violently eager despite melted carefully purple jumping yesterday within happily smashed above quantum briskly toward laughing sideways beneath furiously eating whenever sleepy beneath dancing smoothly around between loudly sparkling after gently during breakfast inside leaping softly because zigzagging upward while before quickly alongside mellow instantly outside nervously beyond slippery since wobbling together anxiously quietly"
 const game = new TypeRacer(words)
 
 const caret = document.createElement("div");
